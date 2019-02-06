ad3eb45239280d1fb21830bda94954b27c771b64
David Hirst <dhirst@ametroslearning.com>
2019-02-06 09:08:17 -0500

    basic scraper working

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,4 +1,25 @@
 class LinksController < ApplicationController
     def index
+        @link = Link.new
+    end
+
+    def show
+        @link = Link.find(params[:id])
+    end
+    
+    def create
+        @link = Link.new(links_params)
+        require 'open-uri'
+        doc = Nokogiri::HTML(open(@link.link))
+        @link.body = doc.css("p").text
+        @link.title = doc.css("h1").text
+        @link.save
+        redirect_to @link
+    end
+
+    private
+
+    def links_params
+        params.require(:link).permit(:link)
     end
 end
